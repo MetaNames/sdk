5ea1e4238c9d73a4b77e5e6748fc5bc50fdfa309
Yeboster <marco@yeboster.com>
2023-04-16 15:25:31 +0200

    feat: update config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,11 @@
-export const testNetRpcConfig = {
-  urlBaseGlobal: { url: 'https://node1.testnet.partisiablockchain.com', shard_id: 99 },
-  urlBaseShards: [
-    { url: 'https://node1.testnet.partisiablockchain.com/shards/Shard0', shard_id: 0 },
-    { url: 'https://node1.testnet.partisiablockchain.com/shards/Shard1', shard_id: 1 },
-    { url: 'https://node1.testnet.partisiablockchain.com/shards/Shard2', shard_id: 2 },
-  ],
+export const testNetConfig = {
+  contractAddress: '02085dec3ccfc0a573289db11b4dd703b39f1614a2',
+  rpcConfig: {
+    urlBaseGlobal: { url: 'https://node1.testnet.partisiablockchain.com', shard_id: 99 },
+    urlBaseShards: [
+      { url: 'https://node1.testnet.partisiablockchain.com/shards/Shard0', shard_id: 0 },
+      { url: 'https://node1.testnet.partisiablockchain.com/shards/Shard1', shard_id: 1 },
+      { url: 'https://node1.testnet.partisiablockchain.com/shards/Shard2', shard_id: 2 },
+    ],
+  },
 }
--- test/lookup.ts
+++ test/lookup.ts
@@ -1,17 +1,15 @@
 // TEST LOOKUP OF A DOMAIN
 
 import { MetaNamesContract } from '../src/contract'
-import { testNetRpcConfig } from '../src/config'
+import { testNetConfig } from '../src/config'
 import { RecordClassEnum } from '../src/interface'
 import assert from 'assert'
 
-const addressContract = '02085dec3ccfc0a573289db11b4dd703b39f1614a2'
-const rpcConfig = testNetRpcConfig
 const domain = 'name.meta'
 const recordClass = RecordClassEnum.Wallet
 const expectedLookup = '00373c68dfed999aec39063194e2d3e0870f9899fa'
 
-const metaNamesContract = new MetaNamesContract(addressContract, rpcConfig)
+const metaNamesContract = new MetaNamesContract(testNetConfig.contractAddress, testNetConfig.rpcConfig)
 
 const main = async () => {
   const data = await metaNamesContract.lookUpRecord(domain, recordClass)
