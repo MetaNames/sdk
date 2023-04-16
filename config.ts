4f953a2d3ae943ff16945121bf166a37ea7c5981
Yeboster <marco@yeboster.com>
2023-04-16 12:12:16 +0200

    feat: add testnet config

---
+++ config.ts
@@ -0,0 +1,8 @@
+export const testNetRpcConfig = {
+  urlBaseGlobal: { url: 'https://node1.testnet.partisiablockchain.com', shard_id: 99 },
+  urlBaseShards: [
+    { url: 'https://node1.testnet.partisiablockchain.com/shards/Shard0', shard_id: 0 },
+    { url: 'https://node1.testnet.partisiablockchain.com/shards/Shard1', shard_id: 1 },
+    { url: 'https://node1.testnet.partisiablockchain.com/shards/Shard2', shard_id: 2 },
+  ],
+}
