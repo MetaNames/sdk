89654ab6fef5ec4a9fd329772c97768fc34a9db9
Yeboster <marco@yeboster.com>
2023-07-21 18:04:34 +0200

    fix(tests): catch exceptions

--- src/repositories/domain-repository.ts
+++ src/repositories/domain-repository.ts
@@ -2,17 +2,28 @@ import { actionDomainMintPayload } from "../actions"
 import { IActionDomainMint, IContractRepository } from "../interface"
 import { Domain } from "../models/domain"
 import { getPnsDomains, lookUpDomain } from "../partisia-name-system"
+import DomainValidator from "../validators/domain"
 
 export class DomainRepository {
   contractRepository: IContractRepository
+  domainValidator: DomainValidator
 
   constructor(contractRepository: IContractRepository) {
     this.contractRepository = contractRepository
+    this.domainValidator = new DomainValidator()
   }
 
   async mint(params: IActionDomainMint) {
+    if (!this.domainValidator.validate(params.domain)) throw new Error('Domain validation failed')
+    let normalizedParentDomain: string | undefined
+    if (params.parent_domain) {
+      if (!this.domainValidator.validate(params.parent_domain)) throw new Error('Domain validation failed')
+      normalizedParentDomain = this.domainValidator.normalize(params.parent_domain)
+    }
+
+    const normalizedDomain = this.domainValidator.normalize(params.domain)
     const contractAbi = await this.contractRepository.getContractAbi()
-    const payload = actionDomainMintPayload(contractAbi, params)
+    const payload = actionDomainMintPayload(contractAbi, { ...params, domain: normalizedDomain, parent_domain: normalizedParentDomain })
 
     return await this.contractRepository.createTransaction(payload)
   }
@@ -21,7 +32,9 @@ export class DomainRepository {
     const struct = await this.contractRepository.getState()
     const domains = getPnsDomains(struct)
 
-    const domain = lookUpDomain(domains, domainName)
+    const normalizedDomain = this.domainValidator.normalize(domainName)
+
+    const domain = lookUpDomain(domains, normalizedDomain)
     if (!domain) throw new Error('Domain not found')
 
     return new Domain(domain, this.contractRepository)
--- test/domain.test.ts
+++ test/domain.test.ts
@@ -3,15 +3,18 @@ import { config, mintDomain } from './helpers'
 const domain = 'name.meta'
 
 beforeAll(async () => {
-  const data = await config.metaNamesContract.domainRepository.find(domain)
-  if (!data) await mintDomain(domain)
+  try {
+    await config.metaNamesContract.domainRepository.find(domain)
+  } catch (e) {
+    await mintDomain(domain)
+  }
 }, 15_000)
 
 test('lookup domain', async () => {
   const data = await config.metaNamesContract.domainRepository.find(domain)
 
   expect(data).toBeDefined()
-  expect(data).toHaveProperty('tokenId', 1)
+  expect(data).toHaveProperty('tokenId')
   expect(data).toHaveProperty('parentId')
   expect(data).toHaveProperty('records')
 })
