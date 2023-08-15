d0295d4dbc693a505a947d82292eaafc59561f13
Yeboster <marco@yeboster.com>
2023-08-15 10:44:38 +0200

    feat(config): update testnet contract address

--- src/providers/config/testnet.ts
+++ src/providers/config/testnet.ts
@@ -1,7 +1,7 @@
 import { Config } from "../config"
 
 export const testNetConfig: Config = {
-  contractAddress: '0221f792ea978d62043ba08a625324467a51d9e615',
+  contractAddress: '02bfba8a7eb06a563e2429365290cd2cdabb457668',
   rpcConfig: {
     urlBaseGlobal: { url: 'https://node1.testnet.partisiablockchain.com', shard_id: 99 },
     urlBaseShards: [
