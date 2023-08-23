23daeb1913f05fc2373c6f445815829eb2157609
Yeboster <marco@yeboster.com>
2023-08-23 20:26:59 +0200

    feat: export repositories

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './meta-names'
+export * from './repositories'
 export * from './interface'
 export * from './providers'
---
+++ src/repositories/index.ts
@@ -0,0 +1,4 @@
+export { ContractRepository } from "./contract-repository"
+export { DomainRepository } from "./domain-repository"
+export { RecordRepository } from "./record-repository"
+
