cb28c3190d8ca81a4ec70269da6e413f33c733fe
Yeboster <marco@yeboster.com>
2023-08-23 20:34:55 +0200

    refactor: export meta names contract repo

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,4 +1,5 @@
 export { ContractRepository } from "./contract-repository"
 export { DomainRepository } from "./domain-repository"
 export { RecordRepository } from "./record-repository"
+export { MetaNamesContractRepository } from "./contracts/meta-names-contract-repository"
 
