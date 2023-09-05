ecce88d0406c735dd604b89f8644796a32e9854c
Yeboster <marco@yeboster.com>
2023-09-05 12:34:30 +0200

    feat: unify signing config with strategy

--- src/meta-names-sdk.ts
+++ src/meta-names-sdk.ts
@@ -1,6 +1,7 @@
 import { ContractRepository, DomainRepository } from './repositories'
 import { Config, ConfigProvider, Enviroment } from './providers'
 import { MetaNamesContractRepository } from './repositories/contracts/meta-names-contract-repository'
+import PartisiaSdk from 'partisia-sdk'
 
 /**
  * Meta Names SDK
@@ -20,8 +21,30 @@ export class MetaNamesSdk {
     this.domainRepository = new DomainRepository(this.contractRepository, this.contract, this.config)
   }
 
-  setPrivateKey(privateKey?: string) {
+  /**
+   * Set the strategy to sign transactions
+   * @param strategy Signing strategy
+   * @param value The value of the strategy
+   */
+  setSigningStrategy(strategy: 'privateKey' | 'partisiaSdk', value: string | PartisiaSdk) {
+    if (strategy === 'privateKey') {
+      if (typeof value !== 'string') throw new Error('Private key must be a string')
+
+      this.setPrivateKey(value)
+    } else {
+      if (!(value instanceof PartisiaSdk)) throw new Error('must be an instance of PartisiaSdk')
+
+      this.setPartisiaSdk(value)
+    }
+  }
+
+  private setPrivateKey(privateKey?: string) {
     this.contractRepository.setPrivateKey(privateKey)
     this.contract.setPrivateKey(privateKey)
+  }
+
+  private setPartisiaSdk(partisiaSdk: PartisiaSdk) {
+    this.contractRepository.setPartisiaSdk(partisiaSdk)
+    this.contract.setPartisiaSdk(partisiaSdk)
   }
 }
--- test/helpers/config.ts
+++ test/helpers/config.ts
@@ -7,7 +7,7 @@ const privateKey = `${process.env.TEST_PRIVATE_KEY}`
 if (!privateKey) throw new Error('TEST_PRIVATE_KEY is not set')
 
 const metaNamesSdk = new MetaNamesSdk(Enviroment.testnet)
-metaNamesSdk.setPrivateKey(privateKey)
+metaNamesSdk.setSigningStrategy('privateKey', privateKey)
 
 export const config = {
   address: Buffer.from('00373c68dfed999aec39063194e2d3e0870f9899fa', 'hex'),
