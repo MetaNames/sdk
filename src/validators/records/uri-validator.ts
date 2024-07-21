c598a490f28ff3c43c8c1a64832a05587a3c270e
Marco <marco@yeboster.com>
2024-07-21 17:52:15 +0200

    refactor(validator): add uri

---
+++ src/validators/records/uri-validator.ts
@@ -0,0 +1,25 @@
+import { IRecord, IValidatorOptions } from '../../interface'
+import { DefaultRecordValidator } from './default-validator'
+
+export class UriRecordValidator extends DefaultRecordValidator {
+  validate(record: IRecord, { raiseError }: IValidatorOptions = { raiseError: true }): boolean {
+    try {
+      const data = record.data.toString().trim()
+      new URL(data)
+
+    } catch (error) {
+      this.addError('Invalid URI format')
+    }
+
+    if (raiseError) this.raiseErrors()
+
+    return !this.hasErrors()
+  }
+
+  normalize(record: IRecord): IRecord {
+    const data = record.data.toString().trim()
+    record.data = new URL(data).toString().replace(/\/$/, '')
+
+    return record
+  }
+}
