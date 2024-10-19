2a6fe260deb9c9e023b6a098cc3a250ea547f918
Marco <marco@yeboster.com>
2024-10-19 16:14:03 +0200

    Fix bip32 types (#99)
    * fix: typing, just let ts ignore it
    
    * feat(types): add bip32 typing

--- src/transactions/bip32-path.d.ts
+++ src/transactions/bip32-path.d.ts
@@ -1,5 +1,17 @@
 // Declare the module here as the types do not exist in the bip32-path package
-declare module "bip32-path";
+declare module 'bip32-path' {
+    export default class BIPPath {
+      private path: number[]
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-extraneous-class
-class BIPPath {}
+      constructor(path: number[])
+
+      static validatePathArray(path: number[]): boolean
+      static validateString(text: string, reqRoot?: boolean): boolean
+      static fromPathArray(path: number[]): BIPPath
+      static fromString(text: string, reqRoot?: boolean): BIPPath
+
+      toPathArray(): number[];
+      toString(noRoot?: boolean, oldStyle?: boolean): string
+      inspect(): string
+    }
+}
