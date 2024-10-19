32a3fdc4c2a76da536697dfec5f4c474b5a19e7e
Marco <marco@yeboster.com>
2024-10-19 16:06:23 +0200

    feat(types): add bip32 typing

--- src/transactions/ledger.ts
+++ src/transactions/ledger.ts
@@ -1,5 +1,5 @@
 import { BN } from "@secata-public/bitmanipulation-ts"
-// @ts-ignore - no types available
+// @ts-ignore - no types available when importing in external proj
 import BIPPath from 'bip32-path'
 import type Transport from "@ledgerhq/hw-transport"
 import { Signature } from "./types"
